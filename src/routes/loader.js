185b5b437652334f30e17f08ed2afa51b745b9eb
spest3122 <spest3122@gmail.com>
2021-08-14 23:25:13 +0800

    feat. 今日完成事項 1. 簡化程式碼

--- src/routes/loader.js
+++ src/routes/loader.js
@@ -1,11 +1,7 @@
-import React, { useContext } from 'react'
-import CentralContext from '../tool/central'
+import React from 'react'
 import { withRouter, Route } from 'react-router'
 
 const PendingRouteLoader = (props) => {
-    const context = useContext(CentralContext)
-    console.log(context);
-    console.log(props)
     return (<Route render={()=> props.children} />)
 }
 
